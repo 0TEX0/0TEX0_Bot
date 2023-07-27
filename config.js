b6aef532832196ad12aeee9af7a8d9049dcde043
0TEX0 <94195095+0TEX0@users.noreply.github.com>
2023-07-27 12:26:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     botName: "0TEX0",   // Nom du bot
     botAvatar: "",  // Avatar du bot
     permissions: {
-        status: ["1121377780458860577", "1121377780458860525"], 
+        status: ["1121377780458860577", "1121377780458860525"], // ID des rôles autorisés à faire cette commande
         connect: ["1121377780458860577", "1121377780458860525"],
         players: ["1121377780458860577", "1121377780458860525"],
     }
